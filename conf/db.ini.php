4910a6733c86ca73edea13ceae5cc93180d22dfa
taidmin <494762513@qq.com>
2020-11-22 13:54:55 +0800

    php seckill

---
+++ conf/db.ini.php
@@ -0,0 +1,13 @@
+<?php
+$config['db']['master'] = array(
+    'host' => '127.0.0.1:3306',
+    'user' => 'root',
+    'password' => 'root',
+    'dbname' => 'seckill',
+);
+$config['db']['slave'] = array(
+    'host' => '127.0.0.1:3306',
+    'user' => 'root',
+    'password' => 'root',
+    'dbname' => 'seckill',
+);
